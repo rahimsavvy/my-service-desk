9c1dd33eee04690068819473ab1dc6b8ebb9f84b
Abdul Rahim Hamza <abdulrahim.hamza@openlane.com>
2026-02-13 15:58:05 -0500

    Added Dark Mode functionality

--- script.js
+++ script.js
@@ -109,3 +109,14 @@ function quickSearch(term) {
     
     displayArticles(filtered); // Show the results!
 }
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
